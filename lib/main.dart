602924f85264cf1f2e51547e376aeefd21cee3cc
MarkFathy <markfathy192@gmail.com>
2024-08-19 10:49:02 +0300

    dialogs commit2

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'app.dart';
 
 void main() {
-  // Set the system UI overlay style
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarIconBrightness: Brightness.dark,
