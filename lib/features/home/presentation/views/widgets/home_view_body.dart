e35edc41602a0d5abda668709b8165d3ba800d04
MarkFathy <markfathy192@gmail.com>
2024-09-03 17:42:48 +0300

    adding pharmacies data

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -17,59 +17,66 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final homeCubit = BlocProvider.of<HomeCubit>(context);
-    homeCubit.getHomeData(); // Load both categories and offers
+    homeCubit.getHomeData();
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.025),
-      child: BlocBuilder<HomeCubit, HomeState>(
-        builder: (context, state) {
-          if (state is HomeLoading) {
-            return const CustomCircularProgressIndicator();
-          } else if (state is HomeLoaded) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CustomHomeAppBar(),
-                Text(
-                  S.of(context).categories,
-                  style: AppStyles.sBlack15,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                CategoriesListView(categories: state.categories),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                Text(
-                  S.of(context).offers,
-                  style: AppStyles.sBlack15,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                Expanded(
-                  child: CustomOffersGridView(
-                    items: state.offers.map((offer) => GridViewItemModel(
-                      title: offer.title,
-                      image: offer.image,
-                      oldPrice: offer.oldPrice,
-                      price: offer.price,
-                      clinicName: offer.clinicName,
-                      id:offer.id ,
-                    )).toList(),
-                    buttonText: S.of(context).bookNow,
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return const BookingDialog();
-                        },
-                      );
-                    },
-                  ),
-                ),
-              ],
-            );
-          } else if (state is HomeError) {
-            return Center(child: Text(state.message));
-          }
-          return const SizedBox.shrink();
-        },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CustomHomeAppBar(),
+          Expanded(
+            child: BlocBuilder<HomeCubit, HomeState>(
+              builder: (context, state) {
+                if (state is HomeLoading) {
+                  return const Center(child: CustomCircularProgressIndicator());
+                } else if (state is HomeLoaded) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        S.of(context).categories,
+                        style: AppStyles.sBlack15,
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                      CategoriesListView(categories: state.categories),
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                      Text(
+                        S.of(context).offers,
+                        style: AppStyles.sBlack15,
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                      Expanded(
+                        child: CustomOffersGridView(
+                          items: state.offers.map((offer) => OffersModel(
+                            title: offer.title,
+                            image: offer.image,
+                            oldPrice: offer.oldPrice,
+                            price: offer.price,
+                            clinicName: offer.clinicName,
+                            id: offer.id,
+                          )).toList(),
+                          buttonText: S.of(context).bookNow,
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return const BookingDialog();
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                } else if (state is HomeError) {
+                  return Center(child: Text(state.message));
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
